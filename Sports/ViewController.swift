c21c8267addb331ea81cc84068118a814bc6453f
Macos <macos@192.168.1.6>
2025-05-10 21:17:49 +0300

    First commit

---
+++ Sports/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Sports
+//
+//  Created by Macos on 10/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIPageViewController {
+
+    var pages = [UIViewController]()
+    let pageControl = UIPageControl()//external that's not part of underlying pages
+    let initialPage = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
