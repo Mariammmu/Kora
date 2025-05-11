38c1cb3a2d6ebf812e8ed766ea70c9e55117886e
Macos <macos@192.168.1.6>
2025-05-11 16:10:56 +0300

    Onboarding Screen

--- Sports/ViewController.swift
+++ Sports/ViewController.swift
@@ -7,16 +7,17 @@
 
 import UIKit
 
-class ViewController: UIPageViewController {
+class ViewController: UIViewController  {
+   
+    
 
-    var pages = [UIViewController]()
-    let pageControl = UIPageControl()//external that's not part of underlying pages
-    let initialPage = 0
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+   
 
 }
 
